bb46d8549af2da56e11e51aa89e070fec4b4fbc7
Vamsi Krishna Annavarapu <45938881+VKrishna09@users.noreply.github.com>
2021-11-14 22:04:26 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='aj76ixip',
+version='0.1',
+author='Vamsi Krishna Annavarapu',
+author_email='krishna.annavarapu@fau.de',
+packages=find_packages(),
+install_requires=['numpy', 'Pillow', 'ipywidgets', 'matplotlib'])
